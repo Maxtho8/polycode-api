0e5b564d924771d22f30de9ea94350a11f166f49
Maxtho8 <58566650+Maxtho8@users.noreply.github.com>
2022-05-22 23:18:14 +0200

    add passport module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from "@nestjs/common";
-
 // import { AuthService } from "./auth/auth.service"
 import { UsersModule } from "./users/users.module";
 import { AuthModule } from "./auth/auth.module";
 import { DatabaseModule } from "./database/database.module";
 import { ConfigModule } from "@nestjs/config";
 import { ChallengeModule } from "./challenge/challenge.module";
+import { PassportModule } from "@nestjs/passport";
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, AuthModule, DatabaseModule, ChallengeModule],
+  imports: [ConfigModule.forRoot(), UsersModule, AuthModule, DatabaseModule, ChallengeModule, PassportModule],
   providers: [],
 })
 export class AppModule {}
