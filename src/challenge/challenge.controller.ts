1287f6bc0b5c1305aac268d63a026fb1c25addfa
Maxtho8 <58566650+Maxtho8@users.noreply.github.com>
2022-05-22 23:22:34 +0200

    Update challenge.controller.ts

--- src/challenge/challenge.controller.ts
+++ src/challenge/challenge.controller.ts
@@ -1,22 +1,19 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { ChallengeDto } from "./challenge.dto";
 import { ChallengeService } from "./challenge.service";
-import { UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
 
 @Controller("challenge")
 export class ChallengeController {
   constructor(private challengeService: ChallengeService) {}
 
   //get all challenges json
-  @UseGuards(AuthGuard())
+
   @Get()
   public async getAllChallenges(): Promise<ChallengeDto[]> {
     return this.challengeService.getAllChallenges();
   }
 
   //get challenge json by id
-  @UseGuards(AuthGuard())
   @Get(":id")
   public async getChallenge(@Param("id") id): Promise<ChallengeDto> {
     return this.challengeService.getChallenge(id);
